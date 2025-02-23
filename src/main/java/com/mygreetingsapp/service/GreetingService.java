c9b1947ece13a8ce22f42408b7a3d3af54510c7e
ankit_049 <ankitrajputji00@gmail.com>
2025-02-23 16:53:21 +0530

    [Ankit Rajput] Add. Added the functionality of saving message in repository

--- src/main/java/com/mygreetingsapp/service/GreetingService.java
+++ src/main/java/com/mygreetingsapp/service/GreetingService.java
@@ -1,28 +1,48 @@
 package com.mygreetingsapp.service;
 
+import com.mygreetingsapp.dto.GreetingRequest;
+import com.mygreetingsapp.dto.GreetingResponse;
+import com.mygreetingsapp.entity.GreetingMessage;
+import com.mygreetingsapp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // Service layer to handle business logic
 @Service
 public class GreetingService {
-    // Created a method to get the Greeting message with the given name
-    public String getGreetingMessage(String firstName, String lastName) {
-        // If both first name & last name are provided
-        if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()) {
-            return "Hello, " + firstName + " " + lastName + "!";
-        }
-        // If only first name is provided
-        else if (firstName != null && !firstName.isEmpty()) {
-            return "Hello, " + firstName + "!";
-        }
-        // If only last name is provided
-        else if (lastName != null && !lastName.isEmpty()) {
-            return "Hello, " + lastName + "!";
-        }
-        // If neither is provided, return default message
-        return "Hello, World!";
+    private final GreetingRepository greetingRepository;
+
+    // Constructor-based Dependency Injection
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
     }
 
+    // Generate and Save Greeting Message
+    public GreetingResponse generateAndSaveGreeting(GreetingRequest request) {
+        String message;
+
+        if (request.getFirstName() != null && request.getLastName() != null) {
+            message = "Hello, " + request.getFirstName() + " " + request.getLastName() + "!";
+        } else if (request.getFirstName() != null) {
+            message = "Hello, " + request.getFirstName() + "!";
+        } else if (request.getLastName() != null) {
+            message = "Hello, " + request.getLastName() + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        // Save message in the database
+        GreetingMessage greetingMessage = new GreetingMessage(message);
+        greetingRepository.save(greetingMessage);
+
+        return new GreetingResponse(message,201);
+    }
+
+    // Retrieve All Saved Messages
+    public List<GreetingMessage> getAllGreetings() {
+        return greetingRepository.findAll();
+    }
     // Put Method to  update the data
     public String putGreetingMessage() {
         return "Hello Ankit Rajput, this is a PUT request!";
