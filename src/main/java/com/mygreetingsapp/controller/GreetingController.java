9262f91abe744da50bd9257700156f58217b2fb7
ankit_049 <ankitrajputji00@gmail.com>
2025-02-23 11:46:52 +0530

    [Ankit Rajput] Refactored. GreetingController to use Service Layer for fetching greeting message

--- src/main/java/com/mygreetingsapp/controller/GreetingController.java
+++ src/main/java/com/mygreetingsapp/controller/GreetingController.java
@@ -1,29 +1,41 @@
 package com.mygreetingsapp.controller;
 
 import com.mygreetingsapp.dto.GreetingResponse;
+import com.mygreetingsapp.service.GreetingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
-@RequestMapping("/greeting")
+@RequestMapping("/greeting") // Base URL for all endpoints
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    // Handles GET requests
     @GetMapping
     public GreetingResponse getGreeting() {
-        return new GreetingResponse("Hello Ankit Rajput, this is a GET request!", HttpStatus.OK.value());
+        return new GreetingResponse(greetingService.getGreetingMessage(), HttpStatus.OK.value());
     }
 
+    // Handles POST requests
     @PostMapping
     public GreetingResponse postGreeting() {
-        return new GreetingResponse("Hello Ankit Rajput, this is a POST request!", HttpStatus.CREATED.value());
+        return new GreetingResponse(greetingService.postGreetingMessage(), HttpStatus.CREATED.value());
     }
 
+    // Handles PUT requests
     @PutMapping
     public GreetingResponse putGreeting() {
-        return new GreetingResponse("Hello Ankit Rajput, this is a PUT request!", HttpStatus.OK.value());
+        return new GreetingResponse(greetingService.putGreetingMessage(), HttpStatus.OK.value());
     }
 
+    // Handles DELETE requests
     @DeleteMapping
     public GreetingResponse deleteGreeting() {
-        return new GreetingResponse("Hello Ankit Rajput, this is a DELETE request!", HttpStatus.NO_CONTENT.value());
+        return new GreetingResponse(greetingService.deleteGreetingMessage(), HttpStatus.NO_CONTENT.value());
     }
 }
